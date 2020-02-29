84f03fe4ada5a4c5e88290fe5a6eb5c7daa6fa20
SebastianSzafran <vbdeveloping@outlook.com>
2020-02-29 17:03:31 +0100

    Added first model to gelp grouping information from complex gui elements

---
+++ PleAutomiX/PleAutomiX.Bots.Steps/Models/VillageRow.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace PleAutomiX.Bots.Steps.Models
+{
+    public class VillageRow
+    {
+        public string Name { get; set; }
+
+        public Point Location { get; set; }
+
+        public  int Points { get; set; }
+    }
+}
