4bd06e592b4633d2a9c0a15e95a254d2c6600a8a
SebastianSzafran <vbdeveloping@outlook.com>
2020-07-07 01:07:33 +0200

    Added hardcoded Marcel PlemKey

--- Plemiona/Plemiona.Logic/Services/Registration/RegistrationKeyProvider/ConstantRegistrationKeyProviderService.cs
+++ Plemiona/Plemiona.Logic/Services/Registration/RegistrationKeyProvider/ConstantRegistrationKeyProviderService.cs
@@ -8,6 +8,7 @@ namespace Plemiona.Logic.Services.Registration.RegistrationKeyProvider
         private readonly List<string> _registratedKeys = new List<string>
         {
             "BFEBFBFFBSS-0123456789000906EA", // My key
+            "BFEBFBFFPF0DT3P8000506E3", // Marcel key
         };
 
         public IEnumerable<string> GetRegistratedKeys()
